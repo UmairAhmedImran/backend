748320fce4489f38639527159cd6ca17e6b64508
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2024-08-23 12:18:35 +0500

    Final change

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     # Adjust this to your frontend domain in production
-    allow_origins=["https://polite-rock-0f7ea8200.5.azurestaticapps.net/"],
+    allow_origins=[""],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
